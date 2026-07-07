76e68a43fcba4dbe09cd08827916b714606d0940
Rudan177 <wyjcrtu@gmail.com>
2026-07-07 22:47:06 +0800

    更新 Interface 页面文件

--- Interface/laiyuan.js
+++ Interface/laiyuan.js
@@ -8,51 +8,9 @@ var DOC_DATA = {
     "sections": [
         {
             "id": 0,
-            "label": "标签一",
-            "title": "标签一",
-            "content": "<p>内容待补充。</p>"
+            "label": "null",
+            "title": "null",
+            "content": "<p>null</p>"
         },
-        {
-            "id": 1,
-            "label": "标签二",
-            "title": "标签二",
-            "content": "<p>内容待补充。</p>"
-        },
-        {
-            "id": 2,
-            "label": "标签三",
-            "title": "标签三",
-            "content": "<p>内容待补充。</p>"
-        },
-        {
-            "id": 3,
-            "label": "标签四",
-            "title": "标签四",
-            "content": "<p>内容待补充。</p>"
-        },
-        {
-            "id": 4,
-            "label": "标签五",
-            "title": "标签五",
-            "content": "<p>内容待补充。</p>"
-        },
-        {
-            "id": 5,
-            "label": "标签六",
-            "title": "标签六",
-            "content": "<p>内容待补充。</p>"
-        },
-        {
-            "id": 6,
-            "label": "标签七",
-            "title": "标签七",
-            "content": "<p>内容待补充。</p>"
-        },
-        {
-            "id": 7,
-            "label": "标签八",
-            "title": "标签八",
-            "content": "<p>内容待补充。</p>"
-        }
     ]
 };
