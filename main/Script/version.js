09ba9dba2534db248c065f41757181a7d00e1861
Rudan177 <wyjcrtu@gmail.com>
2026-07-07 15:54:12 +0800

    release: BS158

--- main/Script/version.js
+++ main/Script/version.js
@@ -1,7 +1,7 @@
-const VERSION = "5.2:27-BS157";
-const PACKAGE_ID = "f7e8d9c0a1b2c3d4e5f6789012345678";
+const VERSION = "5.2:27-BS158";
+const PACKAGE_ID = "a3b7c1d9e4f2085617293a4b5c6d7e8f";
 const PACKAGE_FLAG = "Beta";
 const PRODUCT_NAME = "OOOInterface5.2";
-const RELEASE_DATE = "2026年7月6日";
+const RELEASE_DATE = "2026年7月7日";
 const LICENSE_ID = "ABCD-26W08A";
 const COPYRIGHT = "© 2026 ByRUDAN 保留所有权利";
