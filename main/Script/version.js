cfd12cedaaea90402aa0dc1a249ffd4c0e73f719
Rudan177 <wyjcrtu@gmail.com>
2026-07-17 02:09:22 +0800

    RS120.1

--- main/Script/version.js
+++ main/Script/version.js
@@ -1,7 +1,7 @@
-const VERSION = "5.2:25-RS120";
-const PACKAGE_ID = "a1b2c3d4e5f6789012345678abcdef01";
+const VERSION = "5.2.1:25.1-RS120.1";
+const PACKAGE_ID = "h2w3d3j4u8c856719486kwbvlq0c";
 const PACKAGE_FLAG = "Release";
 const PRODUCT_NAME = "OOOInterface5.2";
-const RELEASE_DATE = "2026年6月18日";
+const RELEASE_DATE = "2026年7月17日";
 const LICENSE_ID = "ABCD-26W08A";
 const COPYRIGHT = "© 2026 ByRUDAN 保留所有权利";
