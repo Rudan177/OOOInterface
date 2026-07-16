62d41d4968532f27b77c209bf8b78e49c3387397
Rudan177 <wyjcrtu@gmail.com>
2026-07-17 01:28:48 +0800

    update: README 结构确认

--- main/Script/version.js
+++ main/Script/version.js
@@ -1,8 +1,8 @@
 const VERSION = "5.2:28-BS167";
-const PACKAGE_ID = "0x4E2b8C1d9A7f3B5e";
+const PACKAGE_ID = "0x032FEBeB433c3EAF";
 const PACKAGE_FLAG = "Beta";
 const PRODUCT_NAME = "OOOInterface 28";
-const RELEASE_DATE = "2026年7月13日";
+const RELEASE_DATE = "2026年7月17日";
 const LICENSE_ID = "ABCD-26W08A";
 const COPYRIGHT = "© 2026 ByRUDAN 保留所有权利";
 
