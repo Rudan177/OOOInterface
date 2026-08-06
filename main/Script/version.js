cb9d85bc945926235c66751410f10b7f738988a7
Rudan177 <wyjcrtu@gmail.com>
2026-08-06 21:25:29 +0800

    RS190.1

--- main/Script/version.js
+++ main/Script/version.js
@@ -1,8 +1,8 @@
-const VERSION = "5.2:29-RS190";
+const VERSION = "5.2.29.1:29.1-RS190.1";
 const PACKAGE_ID = "0x3A9f7B2c4E1d8F6a";
 const PACKAGE_FLAG = "Release";
 const PRODUCT_NAME = "OOOInterface 29";
-const RELEASE_DATE = "2026年8月1日";
+const RELEASE_DATE = "2026年8月6日";
 const LICENSE_ID = "ABCD-26W08A";
 const COPYRIGHT = "© 2026 ByRUDAN 保留所有权利";
 
